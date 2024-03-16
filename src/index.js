5512b4b0688760739b4189a515d5332926f39bf8
paulohubert.ph@hotmail.fr <paulohubert.ph@hotmail.fr>
2024-03-16 17:00:15 -0400

    Update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
-const http = require('node:http');
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello World\n');
-});
-server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`);
-});
+const express = require('express')
+const https = require('node:https');
+
+const app = express()
+const port = 3000
+
+const accessToken = "b6df1ac233ea4d359790c9a95ccb1ebb"
+
+app.get('/spotify', (req, res) => {
+	console.log("spotify")
+
+	https.get("https://api.spotify.com/v1/me", {
+		auth: 'Bearer ' + accessToken
+	}, (resSpotify) => {
+		console.log(resSpotify)
+		res.send(resSpotify)
+	});
+	res.send("Hello")
+})
+app.get('/', (req, res) => {
+	console.log("HOME")
+
+	res.send("Hello Home")
+})
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`)
+})
