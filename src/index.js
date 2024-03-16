690dc935270f787922293fc5e1c3eef29170fc9f
paulohubert.ph@hotmail.fr <paulohubert.ph@hotmail.fr>
2024-03-16 17:28:05 -0400

    Bearer handle

--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
 const express = require('express')
-const https = require('node:https');
+const https = require('https');
 
 const app = express()
 const port = 3000
 
-const accessToken = "b6df1ac233ea4d359790c9a95ccb1ebb"
+var accessTokenBearer = ""
+
+
+app.get('/auth', (req, res) => {
+	var client_id = 'b6df1ac233ea4d359790c9a95ccb1ebb';
+	var client_secret = 'dea14dbcfe904185b99bee1d5d75ede5';
+
+	var authOptions = {
+		url: 'https://accounts.spotify.com/api/token',
+		headers: {
+			'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+		},
+		form: {
+			grant_type: 'client_credentials'
+		},
+		json: true
+	};
+
+	https.request(authOptions, function (error, response, body) {
+		if (!error && response.statusCode === 200) {
+			accessTokenBearer = body.access_token;
+		}
+	});
+
+	console.log(accessTokenBearer)
+
+})
+
+
 
 app.get('/spotify', (req, res) => {
+
 	console.log("spotify")
 
-	https.get("https://api.spotify.com/v1/me", {
+
+
+
+	https.get("https://api.spotify.com/v1/playlists/1neO2bS5TBpEWPMerNhl5d", {
 		auth: 'Bearer ' + accessToken
 	}, (resSpotify) => {
 		console.log(resSpotify)
