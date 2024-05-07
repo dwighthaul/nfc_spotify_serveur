0eaa4e0602fc26ad5be5c9aaf323e1d8c9f51ad0
paulohubert.ph@hotmail.fr <paulohubert.ph@hotmail.fr>
2024-05-07 02:02:05 -0400

    MAJ de trucs

--- src/client/src/components/Navbar.js
+++ src/client/src/components/Navbar.js
@@ -49,8 +49,8 @@ function Navbar() {
       <nav className='navbar'>
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            TRVL
-            <i className='fab fa-typo3' />
+            <img src="./images/logo.png" alt="logo" width="50" height="50" />
+            SC
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
