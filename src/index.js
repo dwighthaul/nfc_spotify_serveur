b861b834391e1f020e48ac2fd0f9f0f11a5a6b42
paulohubert.ph@hotmail.fr <paulohubert.ph@hotmail.fr>
2024-05-23 11:01:57 -0400

    Temp

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const app = express();
 // TODO : A garder ?
 
 app.use(function (req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', `*`);
+	res.setHeader('Access-Control-Allow-Origin', `${process.env.CLIENT_ENDPOINT}`);
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 	res.setHeader('Access-Control-Allow-Credentials', true);
