f3982d4da8fc3c281f90abc7f5414fe0d8062620
paulohubert.ph@hotmail.fr <paulohubert.ph@hotmail.fr>
2024-05-24 14:43:27 -0400

    Update des cookies - 2

--- src/index.js
+++ src/index.js
@@ -135,7 +135,7 @@ app.use((req, res, next) => {
 
 
 
-app.get('/getSession', (req, res) => {
+app.get('/getCookies', (req, res) => {
 	console.log('=============');
 	console.log(req.session);
 	console.log('=============');
