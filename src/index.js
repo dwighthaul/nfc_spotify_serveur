392ba2dac4a0a98fbc0a3adbe11296124dbdd138
paulohubert.ph@hotmail.fr <paulohubert.ph@hotmail.fr>
2024-05-24 02:00:26 -0400

    Update des cookies - 2

--- src/index.js
+++ src/index.js
@@ -46,11 +46,12 @@ app.use(
 	session({
 		secret: 'APODAJDSDAJDLFHELSJCPJZXPR',
 		resave: false,
+		key: 'session_cookie_user_auth',
 		saveUninitialized: false,
 		cookie: {
 			secure: false, // Set to true if using HTTPS
 			maxAge: 24 * 60 * 60000,
-			SameSite: 'None'
+			SameSite: 'none',
 		}, // value of maxAge is defined in milliseconds. 
 
 	})
