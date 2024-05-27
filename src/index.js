877c6b252f02d26bfd12e11c2560016cc7692180
paulohubert.ph@hotmail.fr <paulohubert.ph@hotmail.fr>
2024-05-27 00:52:56 -0400

    MAJ

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const app = express();
 
+app.use(function (req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', `https://dwighthaul.com`);
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	res.setHeader('Access-Control-Allow-Credentials', true);
+
+	next();
+});
+
 app.use(cors({
 	origin: 'https://dwighthaul.com', // replace with your frontend domain
 	credentials: true,
 }));
 app.use(cookieParser());
+
+
 
 app.get('/set-cookie', (req, res) => {
 	res.cookie('testCookie', 'testValue', {
