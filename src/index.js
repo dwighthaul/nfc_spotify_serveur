cdfdc029877738335cd78db7ec8192d484e1c6a4
paulohubert.ph@hotmail.fr <paulohubert.ph@hotmail.fr>
2024-05-30 01:14:10 -0400

    TEST

--- src/index.js
+++ src/index.js
@@ -70,5 +70,5 @@ app.get('/get-all-sessions', (req, res) => {
 
 
 app.listen(3000, () => {
-	console.log('Server is running on http://localhost:3000');
+	console.log('Server is running on port:443');
 });
