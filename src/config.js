0878f94ea359b452772463f3a95ff5a5c6774723
paulohubert.ph@hotmail.fr <paulohubert.ph@hotmail.fr>
2024-05-29 23:38:09 -0400

    TEST

--- src/config.js
+++ src/config.js
@@ -3,5 +3,7 @@ const dotenv = require('dotenv');
 
 const pathURL = `./../.env.${process.env.NODE_ENV}`
 
+console.log("pathURL : " + pathURL)
+
 require('dotenv').config({ "path": path.resolve(__dirname, pathURL.trim()) })
 
