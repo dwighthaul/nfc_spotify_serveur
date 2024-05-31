5aa8782ab083bc567993f6cef92896c2355070ec
paulohubert.ph@hotmail.fr <paulohubert.ph@hotmail.fr>
2024-05-31 00:01:24 -0400

    TEST

--- src/index.js
+++ src/index.js
@@ -17,14 +17,16 @@ app.use(function (req, res, next) {
 });
 
 // Session configuration
-app.use(cookieSession({
+app.use(session({
 	secret: 'APODAJDSDAJDLFHELSJCPJZXPR',
 	resave: false,
 	saveUninitialized: true,
-	httpOnly: false, // Helps mitigate XSS attacks by restricting access to the cookie
-	secure: true,  // Ensures the cookie is only sent over HTTPS
-	sameSite: 'none', // Required for cross-site cookies
-	maxAge: 86400000 // 1 day
+	cookie: {
+		httpOnly: true, // Helps mitigate XSS attacks by restricting access to the cookie
+		secure: true,  // Ensures the cookie is only sent over HTTPS
+		sameSite: 'none', // Required for cross-site cookies
+		maxAge: 86400000 // 1 day
+	}
 }));
 
 app.get('/', (req, res) => {
