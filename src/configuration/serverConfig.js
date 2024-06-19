fd8757d52db5e40d726cfb531e85fc340bca65ff
paulohubert.ph@hotmail.fr <paulohubert.ph@hotmail.fr>
2024-06-19 01:23:59 -0400

    Creation du serveur config pour clean le fichier index

---
+++ src/configuration/serverConfig.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const user = require('./../routes/user');
+const spotify = require('./../routes/spotify');
+const userRuntimeDataHandler = require("./../userRuntimeDataHandler");
+const SQLConnection = require('../controller/SQLConnection');
+
+
+class ServerConfig {
+
+	// Construction a partir des configurations
+	constructor(app) {
+		this.app = app;
+	}
+
+	initMiddlewares() {
+		this.app.use(express.static('public'));
+		this.app.use(bodyParser.urlencoded({ extended: false }));
+		this.app.use(bodyParser.json());
+
+		this.app.use((req, res, next) => {
+			res.setHeader('Access-Control-Allow-Origin', `${process.env.CLIENT_ENDPOINT}`);
+			res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+			res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+			res.setHeader('Access-Control-Allow-Credentials', true);
+			next();
+		});
+
+		this.app.set('trust proxy', 1);
+	}
+
+	initSession() {
+		this.app.use(session({
+			secret: 'APODAJDSDAJDLFHELSJCPJZXPR',
+			resave: false,
+			saveUninitialized: true,
+			cookie: {
+				httpOnly: false,
+				secure: (process.env.NODE_ENV === "production"),
+				sameSite: (process.env.NODE_ENV === "production") ? 'None' : 'Strict',
+				maxAge: 86400000 // 1 day
+			}
+		}));
+
+		this.app.use((req, res, next) => {
+			if (req.session?.cookie && req.session.cookie.expires > Date.now()) {
+				userRuntimeDataHandler.delUser();
+			}
+			next();
+		});
+	}
+
+	initRoutes() {
+		this.app.use('/spotify/', spotify);
+		this.app.use('/user/', user);
+
+		this.app.get('/', (req, res) => {
+			res.send(req.session);
+		});
+
+		this.app.get('/test', (req, res) => {
+			res.send({ hello: "world" });
+		});
+	}
+
+	initDatabase() {
+		const c = new SQLConnection();
+		c.connect().then(() => {
+			c.syncDatabase();
+		});
+	}
+
+
+};
+
+module.exports = ServerConfig;
--- src/index.js
+++ src/index.js
@@ -4,71 +4,21 @@ const bodyParser = require('body-parser');
 const SQLConnection = require('./controller/SQLConnection');
 const user = require('./routes/user');
 const spotify = require('./routes/spotify');
-const userRuntimeDataHandler = require("./userRuntimeDataHandler")
+const userRuntimeDataHandler = require("./userRuntimeDataHandler");
+const ServerConfig = require('./configuration/ServerConfig.js');
+require('./config.js');
+
+const app = express();
 
 
-const config = require('./config.js');
+const serverConfig = new ServerConfig(app);
+serverConfig.initMiddlewares();
+serverConfig.initSession();
+serverConfig.initRoutes();
+serverConfig.initDatabase();
 
 const port = process.env.SERVEUR_PORT;
-const app = express();
-app.use(express.static('public'))
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.use(function (req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', `${process.env.CLIENT_ENDPOINT}`);
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-	res.setHeader('Access-Control-Allow-Credentials', true);
-	next();
-});
-
-app.set('trust proxy', 1);
-
-// Session configuration
-app.use(session({
-	secret: 'APODAJDSDAJDLFHELSJCPJZXPR',
-	resave: false,
-	saveUninitialized: true,
-	cookie: {
-		httpOnly: false, // Helps mitigate XSS attacks by restricting access to the cookie
-		secure: (process.env.NODE_ENV === "production"),  // Ensures the cookie is only sent over HTTPS
-		sameSite: (process.env.NODE_ENV === "production") ? 'None' : 'Strict', // Required for cross-site cookies
-		maxAge: 86400000 // 1 day
-	}
-}));
-
-
-
-const c = new SQLConnection();
-c.connect().then(() => {
-	c.syncDatabase()
-})
-
-
-app.use((req, res, next) => {
-	if (req.session?.cookie && req.session.cookie.expires > Date.now()) {
-		// TODO : ça me plait pas trop qu'on ai connaissance ici d'une route encapsulé dans user..
-		//return res.redirect('/user/login');
-		userRuntimeDataHandler.delUser();
-	}
-	next();
-});
-
-
-app.use('/spotify/', spotify);
-app.use('/user/', user);
-
-app.get('/', (req, res) => {
-	res.send(req.session);
-})
-
-
-app.get('/test', (req, res) => {
-	//console.log("TOTO") 
-	res.send({ hello: "world" });
-})
 
 
 app.listen(port, '0.0.0.0', () => {
