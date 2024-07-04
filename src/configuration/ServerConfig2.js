ecfef1df32b26fe861be6b424b1d09b2b7da70a8
paulohubert.ph@hotmail.fr <paulohubert.ph@hotmail.fr>
2024-07-04 01:47:55 -0400

    Update

--- src/configuration/ServerConfig2.js
+++ src/configuration/ServerConfig2.js
@@ -20,7 +20,7 @@ class ServerConfig {
 		this.app.use(express.static('public'));
 		this.app.use(bodyParser.urlencoded({ extended: false }));
 		this.app.use(bodyParser.json());
-
+		console.log("Auth depuis le chemin suivant : ", process.env.CLIENT_ENDPOINT)
 		this.app.use((req, res, next) => {
 			res.setHeader('Access-Control-Allow-Origin', `${process.env.CLIENT_ENDPOINT}`);
 			res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
