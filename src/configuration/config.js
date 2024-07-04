7faa84324275017af528f963a4b2be4df6944e5e
paulohubert.ph@hotmail.fr <paulohubert.ph@hotmail.fr>
2024-07-04 02:02:13 -0400

    Update

--- src/configuration/config.js
+++ src/configuration/config.js
@@ -3,10 +3,11 @@ const path = require('path');
 console.log("process.env.NODE_ENV : " + process.env.NODE_ENV)
 
 const NODE_ENV = process.env.NODE_ENV.trim()
+console.log("Lancement du serveur avec la config suivante : " + NODE_ENV + '--')
 
 const pathURL = `./../.env.${NODE_ENV}`
 
-console.log("Lancement du serveur : " + pathURL)
+console.log("Lancement du serveur avec la config suivante : " + pathURL + '--')
 
-require('dotenv').config({ "path": path.resolve(__dirname, pathURL.trim()) })
+require('dotenv').config({ "path": path.resolve(__dirname, pathURL) })
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
-require('./configuration/config.js');
+const path = require('path');
+
+console.log("process.env.NODE_ENV : " + process.env.NODE_ENV)
+
+const NODE_ENV = process.env.NODE_ENV.trim()
+console.log("Lancement du serveur avec la config suivante : " + NODE_ENV + '--')
+
+const pathURL = `./../.env.${NODE_ENV}`
+
+console.log("Lancement du serveur avec la config suivante : " + pathURL + '--')
+
+require('dotenv').config({ "path": path.resolve(__dirname, pathURL) })
+
 const ServerConfig = require('./configuration/ServerConfig2.js');
 
 const app = express();
+
 
 
 
