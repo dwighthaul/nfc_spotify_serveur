1d073f11e3fb5b8c49ae6a98ad4b8a0ed8cdb319
paulohubert.ph@hotmail.fr <paulohubert.ph@hotmail.fr>
2024-07-06 00:17:15 -0400

    UPDATE

--- src/configuration/config.js
+++ src/configuration/config.js
@@ -7,8 +7,6 @@ console.log("Lancement du serveur avec la config suivante : " + NODE_ENV + '--')
 
 const pathURL = `./../../.env.${NODE_ENV}`
 
-console.log("Chargement du fichier de config : ", path.resolve(__dirname, pathURL))
-
 console.log("Lancement du serveur avec la config suivante : " + pathURL + '--')
 
 require('dotenv').config({ "path": path.resolve(__dirname, pathURL) })
